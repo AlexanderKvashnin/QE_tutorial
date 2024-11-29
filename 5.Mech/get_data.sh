4664ab3d344cb190c1a5bb407ed3931e252c3a84
Alexander Kvashnin <agkvashnin@gmail.com>
2024-11-29 15:24:30 +0300

    Changes of files
    change main files

--- 5.Mech/get_data.sh
+++ 5.Mech/get_data.sh
@@ -3,10 +3,10 @@
 for i in 0990 0995 1000 1005 1010
 do
 
-cat output.$i | grep CELL_ -A1 | sed '1~d' | awk '{print $1}' >> data.cell
-cat output.$i | grep "total   stress" -A1 | sed '1~d' | awk '{print $4}' >> data.c11
-cat output.$i | grep "total   stress" -A2 | sed '1~d' | sed '1~d' | awk '{print $5}' >> data.c12
-cat output.$i | grep "total   stress" -A3 | tail -1 | awk '{print $6}' >> data.c13
+cat output.relax.$i | grep alat | awk '{print $5}'| head -1 >> data.cell
+cat output.relax.$i | grep "total   stress" -A1 | sed '1~d' | awk '{print $4}' >> data.c11
+cat output.relax.$i | grep "total   stress" -A2 | sed '1~d' | sed '1~d' | awk '{print $5}' >> data.c12
+cat output.relax.$i | grep "total   stress" -A3 | tail -1 | awk '{print $6}' >> data.c13
 
 
 done
