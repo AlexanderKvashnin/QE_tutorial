b1c723f0bf5ad8d5bcbcfe6927f14afa12a9f963
Alexander Kvashnin <agkvashnin@gmail.com>
2024-12-17 15:05:29 +0300

    Update get_data.sh

--- 5.Mech/get_data.sh
+++ 5.Mech/get_data.sh
@@ -4,8 +4,8 @@ for i in 0990 0995 1000 1005 1010
 do
 
 cat output.relax.$i | grep alat | awk '{print $5}'| head -1 >> data.cell
-cat output.relax.$i | grep "total   stress" -A1 | sed '1~d' | awk '{print $4}' >> data.c11
-cat output.relax.$i | grep "total   stress" -A2 | sed '1~d' | sed '1~d' | awk '{print $5}' >> data.c12
+cat output.relax.$i | grep "total   stress" -A1 | tail -1 |  awk '{print $4}' >> data.c11
+cat output.relax.$i | grep "total   stress" -A2 | tail -1 | awk '{print $5}' >> data.c12
 cat output.relax.$i | grep "total   stress" -A3 | tail -1 | awk '{print $6}' >> data.c13
 
 
